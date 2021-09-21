1c76174111ffdcc29d8bfb94731c0817230756df
Saransh Singhal <saranshsinghal12@outlook.com>
2021-09-21 16:19:27 +0530

    added authprovider

--- src/Components/UploadFile.js
+++ src/Components/UploadFile.js
@@ -1,18 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
+import { v4 as uuidv4 } from "uuid";
+import { storage, database } from "../firebase";
+import Alert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import LinearProgress from "@mui/material/LinearProgress";
 
-const Input = styled("input")({
-  display: "none",
-});
+const Input = styled("input")({ display: "none" });
 
-function UploadFile() {
+function UploadFile(props) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const types = ["video/mp4", "video/webm", "video/ogg"];
 
-  
+  const onChange = (e) => {
+    const file = e?.target?.files[0];
+
+    if (!file) {
+      setError("Please select a file");
+      setTimeout(() => setError(null), 2000);
+      return;
+    }
+
+    if (types.indexOf(file.type) === -1) {
+      setError("Please select a supported(video) file");
+      setTimeout(() => setError(null), 2000);
+      return;
+    }
+
+    if (file.size / (1024 * 1024) > 100) {
+      setError("Selected file is too big!");
+      setTimeout(() => setError(null), 2000);
+      return;
+    }
+
+    const id = uuidv4();
+
+    const uploadTask = storage
+      .ref(`/posts/${props.userData.userId}/${file.name}`)
+      .put(file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        setError(error);
+        setTimeout(() => setError(null), 2000);
+        setLoading(false);
+      },
+      async () => {
+        setLoading(true);
+
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+          let obj = {
+            comments: [],
+            likes: [],
+            pId: id,
+            pUrl: url,
+            uName: props?.userData?.username,
+            uProfile: props?.userData?.profileUrl,
+            userId: props?.userData?.userId,
+            createdAt: database.getCurrentTimeStamp(),
+          };
+
+          database.posts
+            .add(obj)
+            .then(async (docRef) => {
+              console.log(docRef);
+              let res = await database.users
+                .doc(props.userData.userId)
+                .update({ postIds: [...props.userData.postIds, docRef.id] });
+            })
+            .then(() => {
+              setLoading(false);
+            })
+            .catch((err) => {
+              setError(err);
+              setTimeout(() => setError(null), 2000);
+              setLoading(false);
+            });
+        });
+      },
+    );
+  };
 
   return (
     <>
@@ -22,7 +96,7 @@ function UploadFile() {
         <>
           <label htmlFor="contained-button-file">
             <Input
-              accept="image/*"
+              accept="video/*"
               id="contained-button-file"
               multiple
               onChange={onChange}
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-import firebase, { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 import "firebase/compat/storage";
-import { getFirestore, collection, query } from "firebase/firestore";
+import "firebase/compat/firestore";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDMbGyKPHHKApEajVlF3VlAr8WWY9DOVIg",
-  authDomain: "reelsclone-007.firebaseapp.com",
-  projectId: "reelsclone-007",
-  storageBucket: "reelsclone-007.appspot.com",
-  messagingSenderId: "504188072427",
-  appId: "1:504188072427:web:28291764892002f594a9ae",
-});
+let { Firebase_Config } = require("../secrets");
+firebase.initializeApp(Firebase_Config);
 
-export const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+export const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 export const database = {
-  users: query(collection(firestore, "users")),
-  posts: query(collection(firestore, "posts")),
+  users: firestore.collection("users"),
+  posts: firestore.collection("posts"),
   getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp,
 };
 
